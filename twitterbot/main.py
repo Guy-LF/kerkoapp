bf91f73308faf9351e064ff0c2b38dfbe9c5a520
Guy Eakin <84586297+Guy-LF@users.noreply.github.com>
2022-11-15 11:02:07 -0500

    Update main.py
    minor notes

--- twitterbot/main.py
+++ twitterbot/main.py
@@ -3,7 +3,7 @@ import feedparser
 
 #####
 """
-generic twitter posting script with image
+generic SM posting script with image
 
 intent is to 
 1) monitor legato\kerko ATOM feed for new posts
@@ -11,18 +11,22 @@ intent is to
 
 2) compose a standard form text from the ATOM syndication data
     eg  "[first author] - [truncated title ...]  [legato permalink] #lipedema #medtwitter"
-3) include media image -  eg., "NewLipedemaResearch.jpg"
-
+2a) include media image -  eg., "NewLipedemaResearch.jpg"
 2b) bonus points -- edit media image dynamically to include entire citation and/or abstract
 2c) bonus points -- modify or select image dependent on tags (eg, LF Funded, Imaging, Genetics, open access, etc)
 
-4) if multiple new items, space tweets out over time.  Do not tweet all at once.
-4c)  bonus points -- trigger some notification to communications staff to acknowledge that a tweet was made. possibly twitter DM to self. 
+3) Post to SM
+3a) if multiple new items, space posts out over time.  Do not tweet all at once.
+3b)  bonus points -- trigger some notification to communications staff to acknowledge that a tweet was made. possibly twitter DM to self. 
+
+3c) consider cross posting to multiple SM accounts -- eg., LinkedIn - https://pypi.org/project/python3-linkedin/
 """
 ####
   
-  
-def main():
+
+    
+#probably rewrite this as a class , incorporating auth and posting as attributes/fxns     
+def auth_twitter():
     twitter_auth_keys = {
         "consumer_key"        : "REPLACE_THIS_WITH_YOUR_CONSUMER_KEY",
         "consumer_secret"     : "REPLACE_THIS_WITH_YOUR_CONSUMER_SECRET",
@@ -39,17 +43,18 @@ def main():
             twitter_auth_keys['access_token_secret']
             )
     api = tweepy.API(auth)
- 
+    return(api)
 
-    #need section to iterate over docket, extract info, and compose tweet
-    
-    # Upload image
+    #need section to iterate over docket, extract info, and compose post
+
+def post_twitter():
+    # Upload image  (this may need to be platform specific)
     media = api.media_upload("lf_legato.jpg")
  
     # Post tweet with image
     tweet = "this is test tweet"
     post_result = api.update_status(status=tweet, media_ids=[media.media_id])
-
+    return()
     
 
 def getdocket():
@@ -71,12 +76,13 @@ def getdocket():
             )
     return docket
 
-def compose_tweet(docket_item):
-    """return text, media intended for posting to twitter
+def compose_post(docket_item):
+    """return text, media intended for posting to social media
     likey needs to pick different media depending on whether project is LF funded or not (maybe other tags as well) 
     """
     return
 
 
 if __name__ == "__main__":
-    main()
+    docket = getdocket()
+    main_twitter()
