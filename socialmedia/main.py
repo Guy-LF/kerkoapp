dd1c09fa4addbb9fb2234ce628ef05c928c04ea3
Guy Eakin <84586297+Guy-LF@users.noreply.github.com>
2022-12-30 20:04:45 -0500

    Update main.py

--- socialmedia/main.py
+++ socialmedia/main.py
@@ -19,78 +19,13 @@ intent is to
 """
 ####
 
-import tweepy
+
 import feedparser
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-#probably rewrite this as a class , incorporating auth and posting as attributes/fxns     
-class Twitter():
-    def __init__(self):
-        self.api = self.auth()
-        return
-    def auth(self):
-        twitter_auth_keys = {
-            "consumer_key"        : os.getenv("consumer_key"),
-            "consumer_secret"     : os.getenv("consumer_secret"),
-            "access_token"        : os.getenv("access_token"),
-            "access_token_secret" : os.getenv("access_token_secret")
-        }
-
-        auth = tweepy.OAuth1UserHandler(
-                twitter_auth_keys['consumer_key'],
-                twitter_auth_keys['consumer_secret']
-                )
-        auth.set_access_token(
-                twitter_auth_keys['access_token'],
-                twitter_auth_keys['access_token_secret']
-                )
-        self.api = tweepy.API(auth)
-        return(self.api)
-
-    #necessary only for creating testing accounts
-    def kill_recent(self):
-        """destroy recent posts"""
-        recent = self.api.user_timeline()
-        for x in recent:
-            self.api.destroy_status(x.id)
-        return(True)
-    #necessary only for creating testing accounts
-    def kill_favorites(self):
-        recent = self.api.get_favorites()
-        for x in recent:
-            self.api.destroy_favorite(x.id)
-        return(True)
-    
-    def post(self,open_access=False, funded=False, add_image=True, text="This is a test"):
-        # Upload image  (this may need to be platform specific)
-        
-        if add_image:
-            media = self.api.media_upload("lf_legato.png")
-            if open_access:
-                media = self.api.media_upload("lf_legato_oa.png")
-            elif funded:
-                media = self.api.media_upload("lf_legato_funded.png")
-            elif (funded and open_access):
-                media = self.api.media_upload("lf_legato_funded_oa.png")
-        else:
-            media = None
-
-        # Post tweet with image
-        if media:
-            post_result = self.api.update_status(status=text, media_ids=[media.media_id])
-        else:
-            post_result = self.api.update_status(status=text)
-        return(True)
-    
-#class LinkedIn():
-#    #same as twitter
-#    def auth():
-#        return
-#    def post():
-#        return
-    
+from platforms import Twitter
     
 def getdocket():
     """get ATOM feeds and isolate postable information"""
