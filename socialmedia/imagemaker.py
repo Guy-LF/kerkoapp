717c056a1073feb1d984ae8f6edc05b1a5f544bd
Guy Eakin <84586297+Guy-LF@users.noreply.github.com>
2023-01-01 17:53:27 -0500

    Update imagemaker.py

--- socialmedia/imagemaker.py
+++ socialmedia/imagemaker.py
@@ -19,12 +19,15 @@ def format_text(text=None):
         text = "0123456789"*100 # test string
     return(re.sub("(.{64})", "\\1\n", text, 0, re.DOTALL))
 
-def main():
+def main(title=None, text=None, author=None, year=None):
 
     fontname = "/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf" #or -Bold.ttf  
-    fontsize = 11   
-    text = format_text(("Gross et al. (2015) have demonstrated that about a quarter of hits would typically be lost to keyword searchers" * 10))
+    fontsize = 11 
     
+    if not text:
+        text = format_text(("Gross et al. (2015) have demonstrated that about a quarter of hits would typically be lost to keyword searchers" * 10))
+    
+    full_text = f"{author} {year}\n title: {title}\n {text}"
     colorText = "black"
     colorOutline = "red"
     colorBackground = "white"
@@ -34,7 +37,7 @@ def main():
     width, height = getSize(text, font)
     img = Image.new('RGB', (width+4, height+4), colorBackground)
     d = ImageDraw.Draw(img)
-    d.text((2, height/2), text, fill=colorText, font=font)
+    d.text((2, height/2), fulltext, fill=colorText, font=font)
     d.rectangle((0, 0, width+3, height+3), outline=colorOutline)
     img.thumbnail((600,400))
     img.save("test.png")
