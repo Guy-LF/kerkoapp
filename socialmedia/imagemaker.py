219b518e56383d5cebb102f439a159bf645e8296
Guy Eakin <84586297+Guy-LF@users.noreply.github.com>
2023-01-01 17:04:41 -0500

    Update imagemaker.py

--- socialmedia/imagemaker.py
+++ socialmedia/imagemaker.py
@@ -1,3 +1,8 @@
-to do
-  
-  
+
+#notes. still a lot to do
+import text_to_image
+encoded_image_path = text_to_image.encode(text, "test.png")
+
+a = platforms.Twitter()
+media = a.api.media_upload("test.png")
+a.api.update_status(status="hello", media_ids=[media.media_id])  
