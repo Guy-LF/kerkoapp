4f1cc2e5d1e44ae0889fc23f056e2f2912c6866c
Guy Eakin <84586297+Guy-LF@users.noreply.github.com>
2023-01-01 16:10:54 -0500

    Create socialmediaconfig.py

---
+++ socialmedia/socialmediaconfig.py
@@ -0,0 +1,7 @@
+url="https://demo.kerko.whiskyechobravo.com/bibliography/atom.xml"
+url_funded="https://demo.kerko.whiskyechobravo.com/bibliography/atom.xml?topic=JTZGXUV6"
+url_oa="https://demo.kerko.whiskyechobravo.com/bibliography/atom.xml?topic=Z8LT6QZG"
+max_post = 5 #used for testing, limits number of new records posted to social media
+timeout = 20 #number of seconds of rest between each post
+repeat_post_number = 0 #how many times should the set of posts be repeated? 0 means each item is posted only once. 
+repeat_post_delay = (3600*12) #number of seconds to wait between reposts of the series
