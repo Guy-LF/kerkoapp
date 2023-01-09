b067009fe8fb9f117bcf2ebd7fc2e1809d36914c
Guy Eakin <84586297+Guy-LF@users.noreply.github.com>
2023-01-09 00:28:04 -0500

    Update imagemaker.py

--- socialmedia/imagemaker.py
+++ socialmedia/imagemaker.py
@@ -21,7 +21,7 @@ def format_text(text=None):
     dedented_text = textwrap.dedent(text=text)
     return(wrapper.fill(text=dedented_text))
 
-def add_watermark(img, watermark_path='lf_logo.png', scale = 1.1, opacity=1.0, brightness=1.0 , contrast=2.0):
+def add_watermark(img, watermark_path='lf_logo.png', scale = 1.1, opacity=1.0, brightness=0.9 , contrast=1.8):
     width, height = img.size
  
     #resize watermark file to 50% of base file
